b742e473883a2fe7fecf2ecb35fb73dac5945b98
righ <django.hamster@gmail.com>
2020-09-26 03:16:34 +0900

    5x5

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Component from "./index";
 
 const data = [
-  ["a", "b", "c"],
-  ["d", "e", "f"],
-  ["g", "h", "i"],
-  ["j", "k", "l"],
-  ["m", "n", "o"],
+  ["a", "b", "c", "d", "e"],
+  ["f", "g", "h", "i", "j"],
+  ["k", "l", "m", "n", "o"],
+  ["p", "q", "r", "s", "t"],
+  ["u", "v", "w", "x", "y"],
 ];
 
 const props = {
