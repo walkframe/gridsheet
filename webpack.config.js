6d00c289eec81749b01056dcfdd754844a4ceec8
righ <django.hamster@gmail.com>
2021-01-01 03:26:37 +0900

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,9 @@ const path = require("path");
 
 module.exports = {
   // mode: 'production',
-  entry: './src/index.tsx',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, "dist"),
-    // filename: 'foo.bundle.js'
   },
   module: {
     rules: [
