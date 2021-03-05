f015afa33bdfff94ca2734b80c5bd44c803d5606
righ <django.hamster@gmail.com>
2021-03-05 23:10:23 +0900

    add

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -7,6 +7,9 @@ import {
   ZoneType,
   AreaType,
   PositionType,
+  Renderers,
+  Parsers,
+  RangeType,
 } from "../types";
 import {
   makeSequence,
@@ -17,13 +20,12 @@ import {
   zoneToArea,
   zoneShape,
   superposeArea,
-  slideFlattened,
-  applyFlattened,
-  inverseFlattened,
-  matrixShape,
 } from "../api/arrays";
+import { ParserType } from "../parsers/core";
 
 import * as histories from "../api/histories";
+import { tsv2matrix, n2a } from "../api/converters";
+import { DEFAULT_HEIGHT, DEFAULT_WIDTH } from "../constants";
 
 const actions: { [s: string]: CoreAction<any> } = {};
 
@@ -58,6 +60,50 @@ export class CoreAction<T> {
   }
 }
 
+class InitHistoryAction<T extends number> extends CoreAction<T> {
+  code = "SET_RENDERERS";
+  reduce(store: StoreType, payload: T): StoreType {
+    return {
+      ...store,
+      history: { operations: [], index: -1, size: payload },
+    };
+  }
+}
+export const initHistory = new InitHistoryAction().bind();
+
+class SetEditingCellAction<T extends string> extends CoreAction<T> {
+  code = "SET_EDITING_CELL";
+  reduce(store: StoreType, payload: T): StoreType {
+    return {
+      ...store,
+      editingCell: payload,
+    };
+  }
+}
+export const setEditingCell = new SetEditingCellAction().bind();
+
+class SetRenderersAction<T extends Renderers> extends CoreAction<T> {
+  code = "SET_RENDERERS";
+  reduce(store: StoreType, payload: T): StoreType {
+    return {
+      ...store,
+      renderers: payload,
+    };
+  }
+}
+export const setRenderers = new SetRenderersAction().bind();
+
+class SetParsersAction<T extends Parsers> extends CoreAction<T> {
+  code = "SET_PARSERS";
+  reduce(store: StoreType, payload: T): StoreType {
+    return {
+      ...store,
+      parsers: payload,
+    };
+  }
+}
+export const setParsers = new SetParsersAction().bind();
+
 class SetEnteringAction<T extends boolean> extends CoreAction<T> {
   code = "SET_ENTERING";
   reduce(store: StoreType, payload: T): StoreType {
@@ -67,6 +113,7 @@ class SetEnteringAction<T extends boolean> extends CoreAction<T> {
     };
   }
 }
+export const setEntering = new SetEnteringAction().bind();
 
 class SetHeaderHeightAction<T extends number> extends CoreAction<T> {
   code = "SET_HEADER_HEIGHT";
@@ -77,6 +124,7 @@ class SetHeaderHeightAction<T extends number> extends CoreAction<T> {
     };
   }
 }
+export const setHeaderHeight = new SetHeaderHeightAction().bind();
 
 class SetHeaderWidthAction<T extends number> extends CoreAction<T> {
   code = "SET_HEADER_WIDTH";
@@ -87,6 +135,7 @@ class SetHeaderWidthAction<T extends number> extends CoreAction<T> {
     };
   }
 }
+export const setHeaderWidth = new SetHeaderWidthAction().bind();
 
 class SetSheetHeightAction<T extends number> extends CoreAction<T> {
   code = "SET_SHEET_HEIGHT";
@@ -97,6 +146,7 @@ class SetSheetHeightAction<T extends number> extends CoreAction<T> {
     };
   }
 }
+export const setSheetHeight = new SetSheetHeightAction().bind();
 
 class SetSheetWidthAction<T extends number> extends CoreAction<T> {
   code = "SET_SHEET_WIDTH";
@@ -107,6 +157,7 @@ class SetSheetWidthAction<T extends number> extends CoreAction<T> {
     };
   }
 }
+export const setSheetWidth = new SetSheetWidthAction().bind();
 
 class SetMatrixAction<T extends MatrixType> extends CoreAction<T> {
   code = "SET_MATRIX";
@@ -117,6 +168,7 @@ class SetMatrixAction<T extends MatrixType> extends CoreAction<T> {
     };
   }
 }
+export const setMatrix = new SetMatrixAction().bind();
 
 class SetEditorRectAction<T extends RectType> extends CoreAction<T> {
   code = "SET_EDITOR_RECT";
@@ -127,6 +179,7 @@ class SetEditorRectAction<T extends RectType> extends CoreAction<T> {
     };
   }
 }
+export const setEditorRect = new SetEditorRectAction().bind();
 
 class SetResizingRectAction<T extends RectType> extends CoreAction<T> {
   code = "SET_RESIZING_RECT";
@@ -137,6 +190,7 @@ class SetResizingRectAction<T extends RectType> extends CoreAction<T> {
     };
   }
 }
+export const setResizingRect = new SetResizingRectAction().bind();
 
 class SetCellsOptionAction<T extends CellsOptionType> extends CoreAction<T> {
   code = "SET_CELLS_OPTION";
@@ -147,6 +201,7 @@ class SetCellsOptionAction<T extends CellsOptionType> extends CoreAction<T> {
     };
   }
 }
+export const setCellsOption = new SetCellsOptionAction().bind();
 
 class SetCellOptionAction<
   T extends {
@@ -178,6 +233,7 @@ class SetCellOptionAction<
     };
   }
 }
+export const setCellOption = new SetCellOptionAction().bind();
 
 class BlurAction<T extends any> extends CoreAction<T> {
   code = "BLUR";
@@ -188,6 +244,7 @@ class BlurAction<T extends any> extends CoreAction<T> {
     };
   }
 }
+export const blur = new BlurAction().bind();
 
 class SetEditingAction<T extends string> extends CoreAction<T> {
   code = "SET_EDITING_CELL";
@@ -198,6 +255,7 @@ class SetEditingAction<T extends string> extends CoreAction<T> {
     };
   }
 }
+export const setEditing = new SetEditingAction().bind();
 
 class CopyAction<T extends ZoneType> extends CoreAction<T> {
   code = "COPY";
@@ -209,6 +267,7 @@ class CopyAction<T extends ZoneType> extends CoreAction<T> {
     };
   }
 }
+export const copy = new CopyAction().bind();
 
 class CutAction<T extends ZoneType> extends CoreAction<T> {
   code = "CUT";
@@ -220,6 +279,93 @@ class CutAction<T extends ZoneType> extends CoreAction<T> {
     };
   }
 }
+export const cut = new CutAction().bind();
+
+class PasteAction<
+  T extends { text: string; parser: ParserType }
+> extends CoreAction<T> {
+  code = "PASTE";
+  reduce(store: StoreType, payload: T): StoreType {
+    const { choosing, copyingZone, cutting } = store;
+    let { matrix, selectingZone } = store;
+    const [y, x] = choosing;
+    const selectingArea = zoneToArea(selectingZone);
+    const copyingArea = zoneToArea(copyingZone);
+    const [selectingTop, selectingLeft] = selectingArea;
+    const [copyingTop, copyingLeft] = copyingArea;
+    const [selectingHeight, selectingWidth] = zoneShape(selectingArea);
+    const [copyingHeight, copyingWidth] = zoneShape(copyingArea);
+    const { text, parser } = payload;
+
+    let before: MatrixType = [];
+    let after = cropMatrix(matrix, copyingArea);
+    let height = copyingHeight;
+    let width = copyingWidth;
+    let dst: AreaType;
+    if (cutting) {
+      const blank = spreadMatrix([[""]], copyingHeight, copyingWidth);
+      matrix = writeMatrix(copyingTop, copyingLeft, blank, matrix);
+    }
+    if (selectingTop === -1) {
+      // unselecting destination
+      if (copyingTop === -1) {
+        // unselecting source
+        after = tsv2matrix(text, parser);
+        [height, width] = [after.length - 1, after[0].length - 1];
+      }
+      dst = [y, x, y + height, x + width];
+      before = cropMatrix(matrix, dst);
+      matrix = writeMatrix(y, x, after, matrix);
+      selectingZone =
+        height === 0 && width === 0
+          ? [-1, -1, -1, -1]
+          : [y, x, y + height, x + width];
+    } else {
+      // selecting destination
+      if (copyingTop === -1) {
+        // unselecting source
+        after = tsv2matrix(text, parser);
+        [height, width] = superposeArea(
+          [0, 0, after.length - 1, after[0].length - 1],
+          [0, 0, selectingHeight, selectingWidth]
+        );
+      } else {
+        // selecting source
+        [height, width] = superposeArea(copyingArea, selectingArea);
+      }
+      dst = selectingArea;
+      after = spreadMatrix(after, height, width);
+      before = cropMatrix(
+        matrix,
+        slideArea([0, 0, height, width], selectingTop, selectingLeft)
+      );
+      matrix = writeMatrix(selectingTop, selectingLeft, after, matrix);
+      selectingZone = slideArea(
+        [0, 0, height, width],
+        selectingTop,
+        selectingLeft
+      );
+    }
+    const command =
+      copyingArea[0] !== -1 ? (cutting ? "cut" : "copy") : "write";
+    const history = histories.pushHistory(store.history, {
+      command,
+      dst,
+      src: copyingArea,
+      before,
+      after,
+    });
+    return {
+      ...store,
+      matrix,
+      history,
+      selectingZone,
+      cutting: false,
+      copyingZone: [-1, -1, -1, -1] as ZoneType,
+    };
+  }
+}
+export const paste = new PasteAction().bind();
 
 class EscapeAction<T extends any> extends CoreAction<T> {
   code = "ESCAPE";
@@ -234,6 +380,7 @@ class EscapeAction<T extends any> extends CoreAction<T> {
     };
   }
 }
+export const escape = new EscapeAction().bind();
 
 class ChooseAction<T extends PositionType> extends CoreAction<T> {
   code = "CHOOSE";
@@ -245,6 +392,7 @@ class ChooseAction<T extends PositionType> extends CoreAction<T> {
     };
   }
 }
+export const choose = new ChooseAction().bind();
 
 class SelectAction<T extends ZoneType> extends CoreAction<T> {
   code = "SELECT";
@@ -257,6 +405,46 @@ class SelectAction<T extends ZoneType> extends CoreAction<T> {
     };
   }
 }
+export const select = new SelectAction().bind();
+
+class SelectRowsAction<
+  T extends { range: RangeType; numCols: number }
+> extends CoreAction<T> {
+  code = "SELECT_ROWS";
+  reduce(store: StoreType, payload: T): StoreType {
+    const { range, numCols } = payload;
+    const [start, end] = range.sort();
+    const selectingZone = [start, 0, end, numCols - 1] as ZoneType;
+    return {
+      ...store,
+      selectingZone,
+      choosing: [start, 0] as PositionType,
+      verticalHeadersSelecting: true,
+      horizontalHeadersSelecting: false,
+    };
+  }
+}
+export const selectRows = new SelectRowsAction().bind();
+
+class SelectColsAction<
+  T extends { range: RangeType; numRows: number }
+> extends CoreAction<T> {
+  code = "SELECT_COLS";
+  reduce(store: StoreType, payload: T): StoreType {
+    const { range, numRows } = payload;
+    const [start, end] = range.sort();
+    const selectingZone = [0, start, numRows - 1, end] as ZoneType;
+
+    return {
+      ...store,
+      selectingZone,
+      choosing: [0, start] as PositionType,
+      verticalHeadersSelecting: false,
+      horizontalHeadersSelecting: true,
+    };
+  }
+}
+export const selectCols = new SelectColsAction().bind();
 
 class DragAction<T extends PositionType> extends CoreAction<T> {
   code = "DRAG";
@@ -266,6 +454,7 @@ class DragAction<T extends PositionType> extends CoreAction<T> {
     return { ...store, selectingZone };
   }
 }
+export const drag = new DragAction().bind();
 
 class SearchAction<T extends number> extends CoreAction<T> {
   code = "SEARCH";
@@ -280,6 +469,7 @@ class SearchAction<T extends number> extends CoreAction<T> {
     return { ...store, matchingCells, matchingCellIndex };
   }
 }
+export const search = new SearchAction().bind();
 
 class WriteAction<T extends any> extends CoreAction<T> {
   code = "WRITE";
@@ -303,6 +493,7 @@ class WriteAction<T extends any> extends CoreAction<T> {
     };
   }
 }
+export const write = new WriteAction().bind();
 
 class ClearAction<T extends any> extends CoreAction<T> {
   code = "CLEAR";
@@ -332,6 +523,7 @@ class ClearAction<T extends any> extends CoreAction<T> {
     };
   }
 }
+export const clear = new ClearAction().bind();
 
 class UndoAction<T extends any> extends CoreAction<T> {
   code = "UNDO";
@@ -369,6 +561,7 @@ class UndoAction<T extends any> extends CoreAction<T> {
     return store;
   }
 }
+export const undo = new UndoAction().bind();
 
 class RedoAction<T extends any> extends CoreAction<T> {
   code = "REDO";
@@ -406,12 +599,141 @@ class RedoAction<T extends any> extends CoreAction<T> {
     return store;
   }
 }
-
-export const search = new SearchAction().bind();
-export const setEntering = new SetEnteringAction().bind();
-
-export const drag = new DragAction().bind();
-export const write = new WriteAction().bind();
-export const clear = new ClearAction().bind();
-export const undo = new UndoAction().bind();
 export const redo = new RedoAction().bind();
+
+class ArrowAction<
+  T extends {
+    shiftKey: boolean;
+    deltaY: number;
+    deltaX: number;
+    numRows: number;
+    numCols: number;
+  }
+> extends CoreAction<T> {
+  code = "ARROW";
+  reduce(store: StoreType, payload: T): StoreType {
+    const { shiftKey, deltaY, deltaX, numRows, numCols } = payload;
+    let { choosing, selectingZone, cellsOption } = store;
+    const [y, x] = choosing;
+    if (shiftKey) {
+      let [dragEndY, dragEndX] = [
+        selectingZone[2] === -1 ? y : selectingZone[2],
+        selectingZone[3] === -1 ? x : selectingZone[3],
+      ];
+      const [nextY, nextX] = [dragEndY + deltaY, dragEndX + deltaX];
+      if (nextY < 0 || numRows <= nextY || nextX < 0 || numCols <= nextX) {
+        return store;
+      }
+      selectingZone =
+        y === nextY && x === nextX ? [-1, -1, -1, -1] : [y, x, nextY, nextX];
+      return {
+        ...store,
+        selectingZone,
+      };
+    }
+    const [nextY, nextX] = [y + deltaY, x + deltaX];
+    if (nextY < 0 || numRows <= nextY || nextX < 0 || numCols <= nextX) {
+      return store;
+    }
+    let [editorTop, editorLeft, height, width] = store.editorRect;
+    const defaultHeight = cellsOption.default?.height || DEFAULT_HEIGHT;
+    const defaultWidth = cellsOption.default?.width || DEFAULT_WIDTH;
+    if (deltaY > 0) {
+      for (let i = y; i < nextY; i++) {
+        editorTop += cellsOption[i + 1]?.height || defaultHeight;
+      }
+    } else if (deltaY < 0) {
+      for (let i = y - 1; i >= nextY; i--) {
+        editorTop -= cellsOption[i + 1]?.height || defaultHeight;
+      }
+    }
+    if (deltaX > 0) {
+      for (let i = x; i < nextX; i++) {
+        editorLeft += store.cellsOption[n2a(i + 1)]?.width || defaultWidth;
+      }
+    } else if (deltaX < 0) {
+      for (let i = x - 1; i >= nextX; i--) {
+        editorLeft -= store.cellsOption[n2a(i + 1)]?.width || defaultWidth;
+      }
+    }
+    height = cellsOption[nextY + 1]?.height || defaultHeight;
+    width = cellsOption[n2a(nextX + 1)]?.width || defaultWidth;
+
+    return {
+      ...store,
+      selectingZone: [-1, -1, -1, -1] as ZoneType,
+      choosing: [nextY, nextX] as PositionType,
+      editorRect: [editorTop, editorLeft, height, width] as RectType,
+    };
+  }
+}
+export const arrow = new ArrowAction().bind();
+
+class WalkAction<
+  T extends {
+    deltaY: number;
+    deltaX: number;
+    numRows: number;
+    numCols: number;
+  }
+> extends CoreAction<T> {
+  code = "WALK";
+  reduce(store: StoreType, payload: T): StoreType {
+    const { deltaY, deltaX, numRows, numCols } = payload;
+    let { choosing, selectingZone, cellsOption } = store;
+    const [y, x] = choosing;
+    const selectingArea = zoneToArea(selectingZone);
+    const [top, left, bottom, right] = selectingArea;
+    let [nextY, nextX] = [y + deltaY, x + deltaX];
+    if (nextY < top && top !== -1) {
+      nextY = bottom;
+      nextX = nextX > left ? nextX - 1 : right;
+    }
+    if (nextY > bottom && bottom !== -1) {
+      nextY = top;
+      nextX = nextX < right ? nextX + 1 : left;
+    }
+    if (nextX < left && left !== -1) {
+      nextX = right;
+      nextY = nextY > top ? nextY - 1 : bottom;
+    }
+    if (nextX > right && right !== -1) {
+      nextX = left;
+      nextY = nextY < bottom ? nextY + 1 : top;
+    }
+    if (nextY < 0 || numRows <= nextY || nextX < 0 || numCols <= nextX) {
+      return store;
+    }
+
+    let [editorTop, editorLeft, height, width] = store.editorRect;
+    const defaultHeight = cellsOption.default?.height || DEFAULT_HEIGHT;
+    const defaultWidth = cellsOption.default?.width || DEFAULT_WIDTH;
+    if (deltaY > 0) {
+      for (let i = y; i < nextY; i++) {
+        editorTop += cellsOption[i + 1]?.height || defaultHeight;
+      }
+    } else if (deltaY < 0) {
+      for (let i = y - 1; i >= nextY; i--) {
+        editorTop -= cellsOption[i + 1]?.height || defaultHeight;
+      }
+    }
+    if (deltaX > 0) {
+      for (let i = x; i < nextX; i++) {
+        editorLeft += store.cellsOption[n2a(i + 1)]?.width || defaultWidth;
+      }
+    } else if (deltaX < 0) {
+      for (let i = x - 1; i >= nextX; i--) {
+        editorLeft -= store.cellsOption[n2a(i + 1)]?.width || defaultWidth;
+      }
+    }
+    height = cellsOption[nextY + 1]?.height || defaultHeight;
+    width = cellsOption[n2a(nextX + 1)]?.width || defaultWidth;
+
+    return {
+      ...store,
+      choosing: [nextY, nextX] as PositionType,
+      editorRect: [editorTop, editorLeft, height, width] as RectType,
+    };
+  }
+}
+export const walk = new WalkAction().bind();
