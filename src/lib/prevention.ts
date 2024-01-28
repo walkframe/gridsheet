0b83d3316b08353ec9b09b25c64c24ba330cc193
righ <django.hamster@gmail.com>
2024-01-29 01:39:14 +0900

    update readme

--- src/lib/prevention.ts
+++ src/lib/prevention.ts
@@ -57,3 +57,48 @@ export const isPrevented = (prevention: Prevention | undefined, flag: Prevention
   }
   return (prevention & flag) === flag;
 };
+
+// Don't use this function in production
+export const debugOperations = (prevention: Prevention | undefined) => {
+  const preventions: string[] = [];
+  if (isPrevented(prevention, DeleteRow)) {
+    preventions.push("DeleteRow");
+  }
+  if (isPrevented(prevention, DeleteCol)) {
+    preventions.push("DeleteCol");
+  }
+  if (isPrevented(prevention, AddRowAbove)) {
+    preventions.push("AddRowAbove");
+  }
+  if (isPrevented(prevention, AddRowBelow)) {
+    preventions.push("AddRowBelow");
+  }
+  if (isPrevented(prevention, AddColLeft)) {
+    preventions.push("AddColLeft");
+  }
+  if (isPrevented(prevention, AddColRight)) {
+    preventions.push("AddColRight");
+  }
+  if (isPrevented(prevention, MoveFrom)) {
+    preventions.push("MoveFrom");
+  }
+  if (isPrevented(prevention, MoveTo)) {
+    preventions.push("MoveTo");
+  }
+  if (isPrevented(prevention, Write)) {
+    preventions.push("Write");
+  }
+  if (isPrevented(prevention, Style)) {
+    preventions.push("Style");
+  }
+  if (isPrevented(prevention, Resize)) {
+    preventions.push("Resize");
+  }
+  if (isPrevented(prevention, SetRenderer)) {
+    preventions.push("SetRenderer");
+  }
+  if (isPrevented(prevention, SetParser)) {
+    preventions.push("SetParser");
+  }
+  return preventions;
+}
