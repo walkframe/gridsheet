8f86be3a910932fd46229bbd6b2a712317423a25
righ <django.hamster@gmail.com>
2024-11-17 16:39:30 +0900

    fix: resizer

--- src/components/Resizer.tsx
+++ src/components/Resizer.tsx
@@ -44,7 +44,7 @@ export const Resizer: React.FC = React.memo(() => {
     const diff: CellsByAddressType = {};
     if (x !== -1) {
       let xs = [x];
-      if (leftHeaderSelecting && between({ start: left, end: right }, x)) {
+      if (topHeaderSelecting && between({ start: left, end: right }, x)) {
         xs = makeSequence(left, right + 1);
       }
       xs.forEach((x) => {
@@ -53,7 +53,7 @@ export const Resizer: React.FC = React.memo(() => {
     }
     if (y !== -1) {
       let ys = [y];
-      if (topHeaderSelecting && between({ start: top, end: bottom }, y)) {
+      if (leftHeaderSelecting && between({ start: top, end: bottom }, y)) {
         ys = makeSequence(top, bottom + 1);
       }
       ys.forEach((y) => {
