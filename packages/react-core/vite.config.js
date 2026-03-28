a49f587d7ddf5ef31f222b03f1eac5f4705d11b8
righ <django.hamster@gmail.com>
2026-03-29 00:14:08 +0900

    3.0.0-rc.4

--- packages/react-core/vite.config.js
+++ packages/react-core/vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => ({
         //preserveModulesRoot: process.cwd(),
       },
     },
-    sourcemap: mode === "development",
+    sourcemap: true,
     minify: mode === "development" ? false : "esbuild",
   },
 }));
